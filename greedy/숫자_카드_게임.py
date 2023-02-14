61ff79a004b0537a8f7bcb988c2418532d353cfd
Hungry Developer <49427835+Jack-Snider@users.noreply.github.com>
2023-02-14 22:11:23 +0900

    코드 수정
    코드 수정

--- greedy/숫자_카드_게임.py
+++ greedy/숫자_카드_게임.py
@@ -78,7 +78,7 @@ n,m = map( int, input().split() )
 result = 0
 
 for i in range( n ):
-	data = list( map( int, input().split() ))
+	data = list( map( int, input().split() ) )
 	
 	min_value = min( data )
 	
